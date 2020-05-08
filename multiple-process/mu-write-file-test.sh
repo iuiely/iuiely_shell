037c2c1555d1812761583151f9ae5bdf533f6197
iuiely <30526277+iuiely@users.noreply.github.com>
2020-05-08 10:31:13 +0800

    Update mu-write-file-test.sh

--- multiple-process/mu-write-file-test.sh
+++ multiple-process/mu-write-file-test.sh
@@ -14,12 +14,12 @@ for ((i=0;i<$thread;i++));do
         echo >&10
 done 
  
-for i in a{1..100}
+for i in a{1..200}
 do
         read -u10
         {
         mkdir -p $dir/$i
-        for j in c{1..100}
+        for j in c{1..200}
         do
                 dd if=/dev/zero of=$dir/$i/$j bs=4096 count=1 > /dev/null 2>&1
         done
